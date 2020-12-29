04fda5db7b85328cc96634ebb6a8075ee2075ce2
mmmaxxx <max.webhk@gmail.com>
2020-12-29 16:46:57 +0800

    Server implementation for QRCode based authentication

---
+++ index.js
@@ -0,0 +1,42 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json()
+
+
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render(__dirname + "/views/enterCode.html");
+});
+
+app.get('/:id', (req, res) => {
+
+    console.log('res param', req.params);
+
+    if (req.params.id) {
+        res.render(__dirname + "/views/login.html", {code: req.params.id});
+    } else {
+        res.render(__dirname + "/views/enterCode.html");
+    }
+});
+
+app.post('/success', jsonParser, (req, res) => {
+    console.log('SUCCESS DATA', req.body.code);
+    console.log('SUCCESS DATA', req.body.access_token);
+    io.emit(req.body.code, req.body.access_token);
+    res.send({status: 'ok'})
+
+
+});
+
+
+io.on('connection', (socket) => {
+
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
